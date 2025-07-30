cd2352cefa2b12889b6c3cf87e19b593f567a3ba
Pratap2901 <tupepratap91@gmail.com>
2025-07-30 12:51:58 +0530

    not

--- x.java
+++ x.java
@@ -2,7 +2,6 @@ public class x {
     public static void main(String ar[]){
         System.out.println("Hello x and Me");
         System.out.println("Hello x and Me and aryan");
-        System.out.println("Hello x and Me");
 
     }
 }
